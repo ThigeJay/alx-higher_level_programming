6abcac6a32df91306634cb2d2bdcaad808ec15ea
ThigeJay <thigejames25@gmail.com>
2024-01-04 01:16:03 -0800

    add.py

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    # Assign values to variables
     a = 1
     b = 2
 
-    # Print the result of the addition
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
